5c856232ec4c61fb05c486830ffcf0676ec6a83c
NeilRow56 <neilrowland56@gmail.com>
2024-03-08 06:34:54 +0000

    Dashboard navbar and sidebar complete with theme changer

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+export default async function LandingPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
-    </main>
+    <section className="flex h-[90vh] items-center justify-center bg-background">
+      <div className="relative mx-auto w-full max-w-7xl items-center px-5 md:px-12 lg:px-16 ">
+        <div className="mx-auto max-w-3xl text-center">
+          <div>
+            <span className="w-auto rounded-full bg-secondary px-6 py-3">
+              <span className="text-sm font-medium text-primary">
+                Accountants - Save time and storage costs
+              </span>
+            </span>
+            <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
+              Create working papers with ease
+            </h1>
+            <p className="mx-auto mt-8 max-w-2xl text-base text-secondary-foreground lg:text-xl">
+              Online accounts preparation file. Automatic comparative schedules.
+              Secure cloud storage. Insert spreadsheet, pdf files etc;.
+            </p>
+          </div>
+          <div className="mx-auto mt-10 flex max-w-sm justify-center">
+            <Button asChild>
+              <Link href="/auth/sign-up">Sign Up </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
